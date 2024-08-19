20f26323e699807e2d58ac363ddec106d0de6a09
sarah-shtawi <sarah.sht03@gmail.com>
2024-08-20 00:58:49 +0300

    Student Task MVC

---
+++ TaskStudent/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskStudent.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Update()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+        
+    }
+}
---
+++ TaskStudent/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TaskStudent.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
